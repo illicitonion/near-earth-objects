223a6cfafa101c5aa55fa82bb356c0cde6ee63aa
ellie <ellie.tms1371@gmail.com>
2020-07-21 17:20:07 +0100

    create navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Navbar = () => {
+(
+<div>  
+<nav className="navbar navbar-light bg-light">
+  <a className="navbar-brand" href="#">
+    <img src="" width="30" height="30" class="d-inline-block align-top" />
+  </a>
+</nav>
+</div>  
+)
+}
+
+
+
+export default Navbar;
+
