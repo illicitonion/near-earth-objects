cc4bef5d07c281a8bb5424763cef4bf2e3802a36
ellie <ellie.tms1371@gmail.com>
2020-07-21 17:43:51 +0100

    add navbar contents

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import './App.css'
 const Navbar = () => {
-(
+return (    
 <div>  
-<nav className="navbar navbar-light bg-light">
+<nav className="navbar navbar-light bg-light nav-bg-image">
   <a className="navbar-brand" href="#">
-    <img src="" width="30" height="30" class="d-inline-block align-top" />
+    <img src="https://drjanetwoods.files.wordpress.com/2018/08/1-meteror2.gif" width="100" height="80"/>
   </a>
 </nav>
 </div>  
