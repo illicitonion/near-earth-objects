f733a625125419036fd3fb317d0fa2dc050400c1
ellie <ellie.tms1371@gmail.com>
2020-07-28 23:13:07 +0100

    make my calender component responsive

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -4,35 +4,41 @@ import "../App.css";
 
 const Calender = () => {
   const [data, setData] = useState([]);
-  
   useEffect(() => {
     fetch("https://ssd-api.jpl.nasa.gov/cad.api")
     .then(response => response.json())
     .then(data => setData(data.data)
     );
   }, []);
-
+  let rowNum = 0
   return (
     <div>
-      <table class="table">
-        <thead class="thead-dark">
+      <div className="container"> 
+      <div className="row">
+       <table className="table">
+        <thead className="thead-dark">
           <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Date</th>
+            <th scope="col">#</th>  
+            <th scope="col" className="">Name</th>
+            <th scope="col" className="">Date</th>
           </tr>
         </thead>
         <tbody>
           {data.map((object) => {
             return (
               <tr>
-                <td>{object.map((data) => data.split(","))[0]}</td>
+                <td>{rowNum = rowNum + 1}</td>  
+                <td><a href="#">{object.map((data) => data.split(","))[0]}</a></td>
                 <td>{object.map((data) => data.split(","))[3]}</td>
               </tr>
             );
-          })}
+          })
+          }
         </tbody>
       </table>
+      </div> 
+      </div>
     </div>
   );
-};
+}
 export default Calender;
