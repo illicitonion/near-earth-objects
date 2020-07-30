880a37b01671d9cd1c1a9264e34313d1c4d9c050
ellie <ellie.tms1371@gmail.com>
2020-07-30 21:31:52 +0100

    change the visibility numbers from % to star type

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -77,7 +77,7 @@ const Calender = () => {
                         }
                       </td>
                       <td className="rate">
-                       {Math.ceil((((Math.ceil(object.map((data) => data.split(","))[4] * (1.496e+8).toFixed(2))/5e+6).toFixed(3))/1.5)*100)}%<i class="far fa-star"></i>
+                       {Math.ceil(Math.round((((Math.ceil(object.map((data) => data.split(","))[4] * (1.496e+8).toFixed(2))/5e+6).toFixed(2))/1.5)*100)*0.1)}<i class="fas fa-star"></i>
                       </td>
                     </tr>
                   );
